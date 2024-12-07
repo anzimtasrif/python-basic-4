441232d34c2fe9f863d81d96525866b39539bb9f
Anzim Tasrif <109681624+anzimtasrif@users.noreply.github.com>
2024-12-07 22:57:30 +0600

    Add files via upload

---
+++ input.py
@@ -0,0 +1,20 @@
+#user input
+username = input("enter your username:")
+password = input("enter your passwowrd:")
+print(username)
+print(password)
+
+#type casting
+name = "12354567777"
+print()
+
+print(type(int(name) ))
+
+#List Data type
+li = [1,2,3]
+
+print(li)
+
+li[1]
+
+print(li)
---
+++ list.py
@@ -0,0 +1,28 @@
+#List Data type
+li = [1,2,3]
+
+print(li)
+
+li[1] = 10
+
+print(li)
+
+#Access List Items
+
+Bablu =["channelName","Website","Group","Page","witcher"]
+print(Bablu[1])
+
+#Change List Item
+Bablu[4] = "Linkdin Page"
+print(Bablu)
+
+#append
+
+Bablu.append(10)
+
+print(Bablu)
+
+#insert
+Bablu.insert(5,"Goggle +")
+
+print(Bablu)
